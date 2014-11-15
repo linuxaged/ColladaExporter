44c8c5936d1ce439b4e39650dd733f35139cd010
Tracy Ma <1991md@gmail.com>
2014-11-15 11:37:07 +0800

    first commit

---
+++ test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+    public Mesh rock_5;
+
+    void Start ()
+    {
+        ColladaExporter export = new ColladaExporter("rock_5.dae", true);
+        export.AddGeometry("rock_5", rock_5);
+        export.AddGeometryToScene("rock_5", "Rock");
+        export.Save();
+    }
+
+}
