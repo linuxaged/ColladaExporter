0b51c05cad1b6a38eb7fd3fa5a11c281bb96b017
Ma Di <madi@chukong-inc.com>
2014-11-20 16:25:48 +0800

    impl push_back

--- collada.rs
+++ collada.rs
@@ -2,23 +2,24 @@
 
 use std::mem;
 use std::ptr;
-// use core::default::Default;
 
-type Link<T> = Option<Box<Node<T>>>;
+type NNode<T> = Option<Box<Node<T>>>; // normal node
+type PNode<T> = RawLink<Node<T>>; // pointer node
 
 struct RawLink<T> {
     p: *mut T
 }
 
 struct XmlList<T> {
-    head: Link<T>,
-    tail: RawLink<Node<T>>,
+    head: NNode<T>,
+    tail: PNode<T>,
 }
 
 struct Node<T> {
-    prev: RawLink<Node<T>>,
-    next: Link<T>,
-    data: T,
+	child: NNode<T>,
+    prev: PNode<T>,
+    next: NNode<T>,
+    value: T,
 }
 
 impl<T> RawLink<T> {
@@ -28,45 +29,52 @@ impl<T> RawLink<T> {
 	pub fn some(n: &mut T) -> RawLink<T>{
 		RawLink{ p: n}
 	}
+	pub fn resolve<'a>(&mut self) -> Option<&'a mut T>{
+		if self.p.is_null() {
+			None
+		} else {
+			Some(unsafe {mem::transmute(self.p) })
+		}
+	}
 }
 
 impl<T> Node<T> {
 	pub fn new(v: T) -> Node<T> {
-		Node{prev: RawLink::none(), next: None, data: v}
+		Node{child: None, prev: RawLink::none(), next: None, value: v}
 	}
+}
+
+fn link_pre<T>(mut new_node: Box<Node<T>>, prev: PNode<T>) -> NNode<T> {
+	new_node.prev = prev;
+	Some(new_node)
 }
 
 impl<T> XmlList<T> {
 	pub fn new() -> XmlList<T> {
 		XmlList{head: None, tail: RawLink::none()}
 	}
-	pub fn push_front_node(&mut self, mut new_head: Box<Node<T>>) {
-		match self.head {
+
+	pub fn push_back_node(&mut self, mut new_tail: Box<Node<T>>) {
+		match self.tail.resolve() {
 			None => {
-				self.tail = RawLink::some(&mut *new_head);
-				new_head.prev = RawLink::none();
-				self.head = Some(new_head);
+				self.tail = RawLink::some(&mut *new_tail);
+				self.head = Some(new_tail);
 			}
-			Some(ref mut head) => {
-				new_head.prev = RawLink::none();
-				head.prev = RawLink::some(&mut *new_head);
-				mem::swap(head, &mut new_head); // let the head point to new_head
-				head.next = Some(new_head);
+			Some(tail) => {
+				tail.next = link_pre(new_tail, RawLink::some(tail));
 			}
 		}
 	}
 }
 
-// impl Default for XmlList<T> {
-//   	pub fn default () -> XmlList<T> {
-//     	{XmlList::new()}
-//   	}
-// }
-
-fn main() {
+#[test]
+fn test_insert() {
 	let mut dlist: XmlList<String> = XmlList::new();
-	let node0: Box<Node<String>> = box Node::new("Lily Chen".to_string());
-	let node1: Box<Node<String>> = box Node::new("tracy ma".to_string());
-	dlist.push_front_node(node0);
-	dlist.push_front_node(node1);
+
+	for x in range(0u, 10000u) {
+		let node0: Box<Node<String>> = box Node::new("Lily Chen".to_string());
+		let node1: Box<Node<String>> = box Node::new("tracy ma".to_string());
+		dlist.push_back_node(node0);
+		dlist.push_back_node(node1);
+	}
 }
